47e6d48bc1434f54cd239295c4809188ae33693b
Moxious <moxious@oldhat.org>
2019-03-20 17:50:35 -0400

    working zoomable timeline, sort of

--- timeline/src/App.js
+++ timeline/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
 import Timeline from 'react-visjs-timeline';
 import data from './parsed.json';
 import _ from 'lodash';
 import moment from 'moment';
+import './App.css';
+
+// Assign IDs
+data.forEach((i, idx) => {
+  i.id = idx;
+});
 
 // https://github.com/Lighthouse-io/react-visjs-timeline#readme
+// http://visjs.org/docs/timeline/#Configuration_Options
 const options = {
   width: '100%',
-  height: '450px',
-  stack: false,
-  showMajorLabels: true,
-  // showCurrentTime: true,
-  zoomMin: 1000000,
-  type: 'background',
+  height: '600px',
+  stack: true,
+  align: 'left',
+  margin: {
+    axis: 2,
+    item: 2,
+  },
   format: {
     minorLabels: {
       minute: 'h:mma',
@@ -30,6 +37,10 @@ const labelAddress = addr => {
   return addr;
 };
 
+const assignClass = e => {
+  return e.logLevel;
+};
+
 const generateContent = e => {
   const t = e.eventType;
 
@@ -38,13 +49,16 @@ const generateContent = e => {
 
 class App extends Component {
   state = {
-    eventLimit: 100,
+    eventLimit: 300,
+    selected: null,
   };
 
   componentWillMount() {
     this.palette = [
       'one', 'two', 'three', 'four', 'five',
     ];
+
+    this.buildFilters();
   }
 
   assignColor(i) {
@@ -63,17 +77,38 @@ class App extends Component {
     return i;
   }
 
+  buildFilters() {
+    const members = _.uniq(data.map(i => _.get(i, 'member.raft')).filter(x => x)).sort();
+    const logLevels = _.uniq(data.map(i => i.logLevel).filter(x => x)).sort();
+    const eventTypes = _.uniq(data.map(i => i.eventType).filter(x => x)).sort();
+    const designators = _.uniq(data.map(i => i.classDesignator).filter(x => x)).sort();
+
+    const filters = {
+      members, logLevels, eventTypes, designators,
+    };
+
+    console.log('Filters', filters);
+    this.setState({ filters });
+  }
+
   getGroups() {
     const allBolt = _.uniq(data.map(d => _.get(d.member, 'raft')).filter(x => x)).sort();
 
     return allBolt.map((boltAddr, idx) => ({ 
-      id: idx, 
+      id: labelAddress(boltAddr), 
       content: labelAddress(boltAddr), 
     }));
   }
 
-  clickHandler = (...e) => {
+  clickHandler = (e) => {
     console.log('Click', e);
+    const idx = e.item;
+    const fullItem = data[idx];
+    console.log(fullItem);
+
+    this.setState({
+      selected: fullItem,
+    });
   };
 
   getItems() {
@@ -84,32 +119,54 @@ class App extends Component {
     // }];
     const finished = data
       .filter(d => (
-        d.eventType !== 'unknown' &&
+        // d.eventType !== 'unknown' &&
         d.eventType !== 'PROCEDURE' && 
+        d.eventType !== 'STOP_THE_WORLD' && 
         d.eventType !== 'CONFIGURATION'))
       .slice(0, this.state.eventLimit)
       .map((e, idx) => { 
-        e.id = idx;
-        e.start = moment.utc(e.start).toDate();
-        e.end = moment.utc(e.start).add(5, 'minutes').toDate();
-        e.content = generateContent(e);
-        e.group = labelAddress(_.get(e.member, 'raft'));
-        this.assignColor(e);
-        return e;
+        const item = {
+          id: e.id,
+          start: moment.utc(e.timestamp).toDate(),
+          // end: moment.utc(e.timestamp).add(10, 'seconds').toDate(),
+          content: generateContent(e),
+          group: labelAddress(_.get(e.member, 'raft')),
+          editable: false,
+          className: assignClass(e),
+        };
+        return item;
       });
 
     console.log(finished);
     return finished;
   }
 
-  render() {
+  legend() {
     return (
-      <div className="App">
+      <div style={{textAlign: 'left'}}>
+        Selected: 
+        <pre>{ this.state.selected ? JSON.stringify(this.state.selected, null, 2) : '' }</pre>
+      </div>
+    );
+  }
+
+  timeline() {
+    return (
+      // <div style={{paddingTop: '200px'}}>
         <Timeline 
             options={options} 
             clickHandler={this.clickHandler}
             groups={this.getGroups()} 
             items={this.getItems()}/>
+      // </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.timeline() }
+        {/* { this.legend() } */}
       </div>
     );
   }
